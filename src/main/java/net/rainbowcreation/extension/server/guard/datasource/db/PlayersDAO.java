59de0511a6d9aabea356c064e999da48cf9485a1
ampna <ampnattahapat@gmail.com>
2023-12-06 22:10:41 +0700

    add requiem sleep

--- src/main/java/net/rainbowcreation/extension/server/guard/datasource/db/PlayersDAO.java
+++ src/main/java/net/rainbowcreation/extension/server/guard/datasource/db/PlayersDAO.java
@@ -33,11 +33,11 @@ public class PlayersDAO implements IPlayersDAO<IPlayer> {
   private static final String BANNED_COLUMN = "banned";
   
   public PlayersDAO(IConnectionFactory connectionFactory) throws SQLException {
-    this("players", connectionFactory);
+    this("player", connectionFactory);
   }
   
   public PlayersDAO(IConnectionFactory connectionFactory, Map<String, String> columns) throws SQLException {
-    this.table = "players";
+    this.table = "player";
     this.columns = columns;
     this.connectionFactory = connectionFactory;
     checkTable();
@@ -47,7 +47,7 @@ public class PlayersDAO implements IPlayersDAO<IPlayer> {
     this.table = table;
     this.connectionFactory = connectionFactory;
     this.columns = new HashMap<>();
-    this.columns.put("email", "email");
+    //this.columns.put("email", "email");
     this.columns.put("banned", "banned");
     this.columns.put("password", "password");
     this.columns.put("username", "realname");
@@ -59,10 +59,10 @@ public class PlayersDAO implements IPlayersDAO<IPlayer> {
     try(Connection connection = this.connectionFactory.getConnection(); 
         
         PreparedStatement stmt = connection.prepareStatement(
-          String.format("SELECT %s,%s,%s,%s,%s FROM %s", new Object[] { this.columns.getOrDefault("email", "email"), this.columns
+          String.format("SELECT %s,%s,%s,%s FROM %s", new Object[] { this.columns
               .getOrDefault("banned", "banned"), this.columns
               .getOrDefault("password", "password"), this.columns
-              .getOrDefault("username", "username"), this.columns
+              .getOrDefault("username", "realname"), this.columns
               .getOrDefault("uuid", "uuid"), this.table }))) {
       stmt.executeQuery();
     } catch (SQLException e) {
@@ -72,19 +72,19 @@ public class PlayersDAO implements IPlayersDAO<IPlayer> {
   }
   
   public void create(IPlayer player) throws SQLException {
-    String query = String.format("INSERT INTO %s(%s, %s, %s, %s) VALUES(?, ?, ?, ?)", new Object[] { this.table, this.columns.get("email"), this.columns
+    String query = String.format("INSERT INTO %s(%s, %s, %s) VALUES(?, ?, ?)", new Object[] { this.table, this.columns
           .get("password"), this.columns
           .get("username"), this.columns
           .get("uuid") });
     try(Connection conn = this.connectionFactory.getConnection(); 
         PreparedStatement stmt = conn.prepareStatement(query)) {
-      stmt.setString(1, player.getEmail());
-      stmt.setString(2, player.getPassword());
-      stmt.setString(3, player.getUsername());
+      //stmt.setString(1, player.getEmail());
+      stmt.setString(1, player.getPassword());
+      stmt.setString(2, player.getUsername());
       if (player.getUuid() == null || player.getUuid().length() == 0) {
-        stmt.setNull(4, 12);
+        stmt.setNull(3, 12);
       } else {
-        stmt.setString(4, player.getUuid());
+        stmt.setString(3, player.getUuid());
       } 
       stmt.executeUpdate();
     } 
@@ -94,10 +94,10 @@ public class PlayersDAO implements IPlayersDAO<IPlayer> {
     try(Connection conn = this.connectionFactory.getConnection(); 
         
         PreparedStatement stmt = conn.prepareStatement(
-          String.format("SELECT * FROM %s WHERE %s = ? OR %s = ?", new Object[] { this.table, this.columns.get("email"), this.columns
+          String.format("SELECT * FROM %s WHERE %s = ?", new Object[] { this.table, this.columns
               .get("username") }))) {
-      stmt.setString(1, player.getEmail());
-      stmt.setString(2, player.getUsername());
+      //stmt.setString(1, player.getEmail());
+      stmt.setString(1, player.getUsername());
       ResultSet rs = stmt.executeQuery();
       return (IPlayer)createPlayer(rs);
     } 
@@ -108,7 +108,7 @@ public class PlayersDAO implements IPlayersDAO<IPlayer> {
     if (rs != null && rs.next()) {
       player = new Player();
       player.setBanned((rs.getInt(this.columns.get("banned")) != 0));
-      player.setEmail(rs.getString(this.columns.get("email")));
+      //player.setEmail(rs.getString(this.columns.get("email")));
       player.setPassword(rs.getString(this.columns.get("password")));
       player.setUsername(rs.getString(this.columns.get("username")));
       player.setUuid(rs.getString(this.columns.get("uuid")));
